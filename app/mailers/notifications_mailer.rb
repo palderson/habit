6f86ca097f12123f9e7f6834fd5aedb096eb13ce
Peter Alderson <peter.alderson@gmail.com>
2012-08-18 15:33:42 -0700

    first commit

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,11 @@
+class NotificationsMailer < ActionMailer::Base
+
+  default :from => "mailer@upperlane.com"
+  default :to => "peter@upperlane.com"
+
+  def new_message(message)
+    @message = message
+    mail(:subject => "[GetChan Message] #{message.subject}")
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chan::Application.config.secret_token = '792b3d164064e17dbb2039d52ddd88b9577857c42879c9ce69eb5034070181b678ba14f248f5a56e05bee78f6ef7d7b8ed502bdf7e5ebd5eaf6bf8778f9e3dcd'
